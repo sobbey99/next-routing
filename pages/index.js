50b16742778f8c1a13f2e33518cb1597b7de270d
sobbey99 <sobbey99@gmail.com>
2023-02-19 19:14:10 +0200

    Programatically navigation using router.push

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 function Home() {
+    const router = useRouter()
+    const handleClick = () => {
+        console.log('placing your order.')
+        router.push('/product')
+    }
     return (
         <div>
             <h1>Home page</h1>
@@ -11,6 +17,10 @@ function Home() {
             <Link href='/product'>
                 Products
             </Link>
+            <br />
+            <button onClick={handleClick}>
+                Place order
+            </button>
         </div>
     )
 }
